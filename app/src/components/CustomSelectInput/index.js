61182121e053b02e50113d5aec8889d1bb788578
Fernando Karnagi <fernando@cyder.com.sg>
2019-03-21 09:45:52 +0800

    first update

---
+++ app/src/components/CustomSelectInput/index.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from 'react';
+import { components } from 'react-select';
+
+const CustomSelectInput = props => {
+    var customProps = Object.assign({}, props);
+    delete customProps.autoCorrect;
+    delete customProps.autoCapitalize;
+    return <components.Input {...customProps} />;
+};
+
+export default CustomSelectInput;
---
+++ app/src/components/ReactNotifications/index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import Notifications from './Notifications.js';
+import NotificationContainer from './NotificationContainer';
+import NotificationManager from './NotificationManager';
+
+export { Notifications, NotificationContainer, NotificationManager };
+export default Notifications;
